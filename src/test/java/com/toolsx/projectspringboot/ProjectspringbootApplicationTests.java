06324b472e1bb700db4442c00774f8a35863c674
Cristian Mayorga <cristiandres1317@gmail.com>
2026-01-30 16:35:10 -0500

    feat: :zap: estructura para la inicialización de proyecto

---
+++ src/test/java/com/toolsx/projectspringboot/ProjectspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsx.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
