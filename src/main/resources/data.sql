1dd518de44bbc09647b81f94d657388a5f7a1bb7
Cristian Mayorga <cristiandres1317@gmail.com>
2026-02-05 11:59:57 -0500

    feat: :art: conexion base de datos

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,16 @@
+-- Elimina el usuario si existe
+DROP USER IF EXISTS 'toolsx_user'@'localhost';
 
-DELETE FROM usuarios_roles;
-DELETE FROM usuarios;
+-- Crea el usuario de nuevo
+CREATE USER 'toolsx_user'@'localhost'
+IDENTIFIED BY 'Kaneki#G1317';
 
--- Reiniciar los IDs
-ALTER TABLE usuarios ALTER COLUMN id RESTART WITH 1;
+-- Dale permisos TOTALES sobre la base
+GRANT ALL PRIVILEGES ON toolsx_container_prod.* 
+TO 'toolsx_user'@'localhost';
+
+-- Aplica cambios
+FLUSH PRIVILEGES;
 
 MERGE INTO roles (nombre) KEY(nombre) VALUES('ADMIN');
 MERGE INTO roles (nombre) KEY(nombre) VALUES('CLIENTE');
